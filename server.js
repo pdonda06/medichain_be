2e656f90c69a97c6b5b4a614bac0082094413573
Prince Donda <120329882+pdonda06@users.noreply.github.com>
2025-03-28 23:21:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,15 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: '*',  // Allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 
 // Connect to MongoDB
 mongoose
